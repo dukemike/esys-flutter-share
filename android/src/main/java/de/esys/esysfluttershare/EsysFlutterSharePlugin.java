cef1212a58e2ecfe8a8753c18e84e56faaafc851
Mike Revoir <mike.revoir@duke.edu>
2020-08-25 10:25:54 -0400

    MATCH_ALL rather than filtering the list with MATCH_DEFAULT_ONLY

--- android/src/main/java/de/esys/esysfluttershare/EsysFlutterSharePlugin.java
+++ android/src/main/java/de/esys/esysfluttershare/EsysFlutterSharePlugin.java
@@ -126,7 +126,7 @@ public class EsysFlutterSharePlugin implements MethodCallHandler {
         if (!text.isEmpty()) shareIntent.putExtra(Intent.EXTRA_TEXT, text);
 
         Intent chooser = Intent.createChooser(shareIntent, title);
-        List<ResolveInfo> resInfoList = activeContext.getPackageManager().queryIntentActivities(chooser, PackageManager.MATCH_DEFAULT_ONLY);
+        List<ResolveInfo> resInfoList = activeContext.getPackageManager().queryIntentActivities(chooser, PackageManager.MATCH_ALL);
 
         for (ResolveInfo resolveInfo : resInfoList) {
             String packageName = resolveInfo.activityInfo.packageName;
